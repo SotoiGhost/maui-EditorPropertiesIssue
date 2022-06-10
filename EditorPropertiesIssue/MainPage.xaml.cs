db32319da3a5b459aedb421e1ee1cb328f14c301
Israel Soto <israel.spgh@gmail.com>
2022-06-10 15:29:26 -0500

    Added testcase to repro the issue

---
+++ EditorPropertiesIssue/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace EditorPropertiesIssue;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		txtPassword.CursorPosition = 30;
+		txtPassword.SelectionLength = 10;
+	}
+}
+
